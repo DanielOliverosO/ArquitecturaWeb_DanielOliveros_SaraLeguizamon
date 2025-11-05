56a32666c61cedd2f08d4ba93fa83a10e33c6561
legosmixels <legosmixels@gmail.com>
2025-11-05 17:55:49 -0500

    Creacion del proyecto con su encarpetado, con el patron hexagonal

---
+++ src/main/java/camilleBlonch/CamilleBlonch/CamilleBlonchApplication.java
@@ -0,0 +1,13 @@
+package camilleBlonch.CamilleBlonch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamilleBlonchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamilleBlonchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/camilleBlonch/CamilleBlonch/CamilleBlonchApplicationTests.java
@@ -0,0 +1,13 @@
+package camilleBlonch.CamilleBlonch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilleBlonchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
